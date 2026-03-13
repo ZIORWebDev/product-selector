9275f0735619cb207d470448e973069128c380f6
Rey C <reygcalantaol@gmail.com>
2026-03-13 19:09:47 +0800

    Updates

--- dist/ProductSelector-bak.d.ts
+++
@@ -1,5 +0,0 @@
-import type { FC } from "react";
-import type { ProductSelectorProps } from "./types";
-declare const ProductSelector: FC<ProductSelectorProps>;
-export default ProductSelector;
-//# sourceMappingURL=ProductSelector-bak.d.ts.map
--- dist/defaultFetchers.d.ts
+++
@@ -1,3 +0,0 @@
-import type { ProductsListResponse } from './types';
-export declare function defaultFetchProductOptions(search: unknown, productId: unknown): Promise<ProductsListResponse>;
-//# sourceMappingURL=defaultFetchers.d.ts.map
