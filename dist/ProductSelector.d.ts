d34e532b078b1d263d6189579a9d27450dc166ed
Rey C <reygcalantaol@gmail.com>
2026-03-14 09:30:10 +0800

    Updates

---
+++ dist/ProductSelector.d.ts
@@ -0,0 +1,5 @@
+import type { FC } from "react";
+import type { ProductSelectorProps } from "./types";
+declare const ProductSelector: FC<ProductSelectorProps>;
+export default ProductSelector;
+//# sourceMappingURL=ProductSelector.d.ts.map
---
+++ dist/index.d.ts
@@ -0,0 +1,5 @@
+export { default as ProductSelector } from "./ProductSelector";
+export type { ProductAttr, ProductSelectorProps, ProductsListResponse, ProductInformation, FetchProductOptions, FetchProductInformation, } from "./types";
+export { defaultFetchProductOptions } from "./ProductOptions";
+export { defaultFetchProductInformation } from "./ProductInformation";
+//# sourceMappingURL=index.d.ts.map
