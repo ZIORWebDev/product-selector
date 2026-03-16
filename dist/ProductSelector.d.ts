5aa374c93894cb7969b7d16cefa61c27c3c0fddc
Rey C <reygcalantaol@gmail.com>
2026-03-16 18:27:57 +0800

    Updates

---
+++ dist/ProductSelector.d.ts
@@ -0,0 +1,4 @@
+import type { ProductSelectorProps } from "./types";
+declare const ProductSelector: ({ value, onChange, onProductInformationChange, onProductInformationError, fetchOptions, fetchProductInformation, }: ProductSelectorProps) => import("react/jsx-runtime").JSX.Element;
+export default ProductSelector;
+//# sourceMappingURL=ProductSelector.d.ts.map
